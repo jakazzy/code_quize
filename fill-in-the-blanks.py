be6bf8f0305643209684d7da90bbabdd6581c6b8
jida <jakazzy@gmail.com>
2018-04-22 10:51:11 +0000

    added comments t the functions

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -48,7 +48,7 @@ welcome= "Hello " + name+ "!, welcome to fill-in-the-blanks game."
 
 instruction ="This is aimed at testing how well you understand your business lessons. Fill the 5 blank spaces with the correct word. Choose the number of turns to be repeated when you get an answer wrong."
 
-turn = int(raw_input("number of turns?: "))
+turn = int(raw_input("number of turns(choose a number)?: "))
 
 answers =["model", "financial modeling", "predicting", "meteorologist", "assumptions"]
 
@@ -56,14 +56,7 @@ answers =["model", "financial modeling", "predicting", "meteorologist", "assumpt
 
 
 sample ="The word " + '__1__'+ " is often tossed around the business world, evoking complicated and intimidating visual images to those unfamiliar with them. But instead of depending on your friend with an MBA to back you up, any business owner can and should know the basics of " +'__2__' + ". So what is a financial model exactly?A model is a means of " +'__3__' +" the future, and like a " +'__4__'+ " forecasting rain, a financial model is really just a volatile \"best guess\" that should be updated frequently.  Models take a set of " + '__5__'+ " (and sometimes your business's performance history) and forecast a future state. Even though they are predictions, models provide a good benchmark and can help run \"what-if\" scenarios so you are prepared for any situation."
-questions ={
-	"question1":["__1__","model"],
-	"question2":["__2__","financial modeling"],
-	"question3":["__3__","predicting"],
-	"question4":["__4__","meteorologist"],
-	"question5":["__5__","assumptions"],
 
-}
 
 question1= "What should be substituted for __1__?: "
 question2= "What should be substituted for __2__?: "
@@ -82,28 +75,38 @@ blank_spaces = {
 }
 
 blank_space = ["__1__", "__2__","__3__","__4__","__5__"]
-word1 = "__1__"
-word2 = "__2__"
-word3 = "__3__"
-word4 = "__4__"
-word5 = "__5__"
 
 
+#Helper functions
 def ask_question(question):
+	"""Takes in a question, outputs the question using the raw_input function and 
+	returns the answer
+	"""
 	answer = raw_input(question)
 	return answer
 
+
 def check_answer(answer):
+	"""Checks if an answer is correct by taking in a paramenter answer,
+	checking in the list answers and returning True or False
+	"""
 	if answer in answers:
 		return True
 	return False
 
 
 def replace_answer(note,question,answer):
+	"""
+	Replaces a word, question in the sentence or paragraph, note with the word answer
+	and then outputs the rewritten sentence or paragraph
+	"""
 	note = note.replace(question,answer)
 	return note
 
 def wrong_answer(answer, question):
+	"""
+	checks if an answer is wrong, if wrong, repeats it till the answer is correct
+	"""
 	if answer not in answers:
 		print "sorry wrong answer"
 		for i in range(turn):
@@ -122,6 +125,11 @@ print "-"*80
 print welcome, "\n", instruction, "\n\n", sample
 
 def ask_and_answer(list_questions):
+	"""
+	From list of questions, takes a question, accepts response, checks if the 
+	response is correct. If correct, proceeds to other questions. If not correct,
+	ask you number of times(turn) for right answer to be outputed.
+	"""
 	note = sample
 	for question in list_questions:
 		response = ask_question(question)
@@ -138,6 +146,7 @@ def ask_and_answer(list_questions):
 
 
 ask_and_answer(question_list)
+print "credit for resource for quize: https://www.paro.io/blog/how-to-build-a-basic-financial-model"
 # questions = ['name', 'quest', 'favorite color']
 # answers = ['lancelot', 'the holy grail', 'blue']
 # for q, a in zip(questions, answers):
